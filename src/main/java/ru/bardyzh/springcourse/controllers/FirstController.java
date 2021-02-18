62933a64fd13049332487b2acbd42fb8d3a58491
Arseni_Bardij <senia_94@mail.ru>
2021-02-18 19:35:34 +0300

    Lesson 20

--- src/main/java/ru/bardyzh/springcourse/controllers/FirstController.java
+++ src/main/java/ru/bardyzh/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.bardyzh.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,15 +12,13 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(/*HttpServletRequest request,*/
-                            @RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname){
-        /*String name = request.getParameter("name");
-        String surname = request.getParameter("surname");*/
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model){
 
-        System.out.println(("Hello, " + name + " " + surname));
+        //System.out.println(("Hello, " + name + " " + surname));
 
-
+        model.addAttribute("message", "Hello, " + name + " " + surname);
         return "first/hello";
     }
 
